5cc4ca7b289eb318de7194aea794b1e8c39a171e
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-12-24 22:53:18 +0100

    added memory and system prompt

--- src/main/java/ma/lahjaily/agents/AIAgent.java
+++ src/main/java/ma/lahjaily/agents/AIAgent.java
@@ -1,6 +1,8 @@
 package ma.lahjaily.agents;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Flux;
@@ -9,8 +11,16 @@ import reactor.core.publisher.Flux;
 public class AIAgent {
     private ChatClient chatClient;
 
-    public AIAgent(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public AIAgent(ChatClient.Builder builder, ChatMemory memory) {
+        this.chatClient = builder
+                .defaultSystem("""
+                        Vous un assistant qui se charge de répondre aux question
+                        de l'utilisateur en fonction du contexte fourni.
+                        Si aucun contexte n'est fourni, répond avec JE NE SAIS PAS.
+                        """)
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(memory).build())
+                .build();
     }
 
     @GetMapping("/chat")
