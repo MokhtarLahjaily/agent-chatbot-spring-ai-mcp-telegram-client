13948f89f4fda14640b8fc22ddbcc7f9796e64c4
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-12-25 00:05:18 +0100

    added telegram bot

--- src/main/java/ma/lahjaily/web/ChatController.java
+++ src/main/java/ma/lahjaily/web/ChatController.java
@@ -17,7 +17,7 @@ public class ChatController {
     }
 
     @GetMapping(value = "/chat", produces = MediaType.TEXT_PLAIN_VALUE)
-    public Flux<String> chat(String query) {
+    public String chat(String query) {
         return aiAgent.askAgent(query);
     }
 }
