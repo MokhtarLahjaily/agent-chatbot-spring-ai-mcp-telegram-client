d6951b97620ec1b274d2cf4edbdb6cc0bd6c7f34
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-12-25 02:17:02 +0100

    implemented rag as mcp tool

--- mcp-server/src/main/java/ma/lahjaily/mcpserver/tools/McpTools.java
+++ mcp-server/src/main/java/ma/lahjaily/mcpserver/tools/McpTools.java
@@ -2,12 +2,21 @@ package ma.lahjaily.mcpserver.tools;
 
 import org.springaicommunity.mcp.annotation.McpArg;
 import org.springaicommunity.mcp.annotation.McpTool;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
+import org.springframework.ai.document.Document;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class McpTools {
+
+    private VectorStore vectorStore;
+    public McpTools(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
     @McpTool(name = "getEmployee"
             , description = "Get information about a given employee by name")
     public Employee getEmployee(@McpArg(description = "The employee name") String name){
@@ -21,6 +30,17 @@ public class McpTools {
                 new Employee("Bob", 18000.0, 7),
                 new Employee("Charlie", 15000.0, 5)
         );
+    }
+
+    @McpTool(description = "get informations needed from CV context")
+    public List<String> getContext(@McpArg(description = "The users needed informations from the CV") String query){
+        List<Document> documents = vectorStore.similaritySearch(SearchRequest.builder()
+                        .query(query)
+                        .topK(4)
+                .build());
+        return documents.stream()
+                .map(Document::getText)
+                .collect(Collectors.toList());
     }
 
 }
